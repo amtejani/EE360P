08341f60df24ab9d8303a011535fe1a9aa2aea7c
Travis <tmcclure4@yahoo.com>
2017-01-29 17:49:17 -0600

    finished Q2

--- HW1/Q2/src/PSearch.java
+++ HW1/Q2/src/PSearch.java
@@ -1,13 +1,72 @@
-//UT-EID=
+//UT-EID=tam2983
+// amt3639
 
 
 import java.util.*;
 import java.util.concurrent.*;
 
 
-public class PSearch{
-  public static int parallelSearch(int k, int[] A, int numThreads){
-    // TODO: Implement your parallel search function 
+public class PSearch implements Callable<Integer>{
+	 public static ExecutorService threadPool;
+	  private int start;
+	  private int end;
+	  private int k;
+	  private int[] A;
+	  
+	
+	public static int parallelSearch(int k, int[] A, int numThreads){
+	  
+	 
+    
+	  try {
+		  int j=-1;
+          ExecutorService es = Executors.newSingleThreadExecutor();
+          threadPool = Executors.newCachedThreadPool();
+          int numElements=A.length / numThreads;
+          int extras = A.length%numThreads;
+          int i=0;
+          HashSet<Future<Integer>> futureHash = new HashSet();
+          while(i<A.length){
+        	  if(extras > 0){
+        		  futureHash.add(threadPool.submit(new PSearch(k,i,i+numElements+1,A)));  
+        		  extras--;
+        		  i += numElements+1;
+        	  } else {
+        		  futureHash.add(threadPool.submit(new PSearch(k,i,i+numElements,A)));
+        		  i+= numElements;
+        	  }
+          }
+          for(Future<Integer> f: futureHash) {
+        	  j = f.get();
+        	  if(j != -1)
+        		  break;
+        
+          }
+          es.shutdown();
+          PSearch.threadPool.shutdown();
+          return j;
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+	  
     return -1; // if not found
+  }
+  
+  public PSearch(int k, int start, int end, int[] A){
+	  this.k=k;
+	  this.start=start;
+	  this.end=end;
+	  this.A=A;
+  }
+  
+  @Override
+  public Integer call(){
+	  
+	  for(int counter=start; counter<end;counter++){
+		  if(A[counter]==k){//found the number
+			  return counter;
+		  }
+	  }
+	  return -1;
   }
 }
