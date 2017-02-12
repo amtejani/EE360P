97567d74ec9438dcc6d19cc52f11ce6b82a43a4f
Ali Tejani <Ali Tejani>
2017-02-12 15:48:51 -0600

    q2 done

--- HW2/q2/src/FairReadWriteLock.java
+++ HW2/q2/src/FairReadWriteLock.java
@@ -1,15 +1,48 @@
+import org.junit.Test;
+
 public class FairReadWriteLock {
-	
-                        
-	public synchronized void beginRead() {
-	}
-	
-	public synchronized void endRead() {
-	}
-	
-	public synchronized void beginWrite() {
-	}
-	public synchronized void endWrite() {
-	}
+    int numReaders = 0;
+    int accessRequests = 0;
+	int currRequest = 0;
+    boolean reading = false;
+    boolean writing = false;
+
+    public synchronized void beginRead() {
+        int thisReq = accessRequests++;
+        while(writing || thisReq > currRequest) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        currRequest += 1;
+        numReaders++;
+        notify();
+    }
+
+    public synchronized void endRead() {
+        numReaders--;
+        notify();
+    }
+
+    public synchronized void beginWrite() {
+        int thisRequest = accessRequests++;
+        while(numReaders > 0 || writing || thisRequest > currRequest) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        writing = true;
+        currRequest++;
+        notify();
+    }
+
+    public synchronized void endWrite() {
+        writing = false;
+        notify();
+    }
 }
-	
+
